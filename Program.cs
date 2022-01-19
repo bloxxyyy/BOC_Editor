cfc80d912685d5832c0653dcb235a02710a67481
Marco <mn.noppen@hotmail.com>
2022-01-19 16:37:42 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BOC_Editor
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
